382d6e17c929c927326eaa0ef36d24def3586ab0
scrumretrospective <scrumretrospective@gmail.com>
2013-10-23 13:11:34 -0400

    initial version

--- scrumretro-maven/src/main/java/com/scrumretro/service/ProjectService.java
+++ scrumretro-maven/src/main/java/com/scrumretro/service/ProjectService.java
@@ -1,17 +1,16 @@
 package com.scrumretro.service;
 
 import com.scrumretro.model.Item;
-
 /**
- * Service class for Item CURD operation.
  * 
+ *
  */
-public interface ItemService {
+public interface ProjectService {
 
 	/**
+	 * 
 	 * @param item
 	 * @return
 	 */
 	public String save(final Item item);
-	
 }
