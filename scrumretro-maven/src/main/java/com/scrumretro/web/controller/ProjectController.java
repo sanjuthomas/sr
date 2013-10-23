fe5b722de2c8e21bd6c1ad1f5eafd160255a8178
Sanju Thomas <sanju@sanju.org>
2013-10-23 15:05:48 -0400

    initial version

--- scrumretro-maven/src/main/java/com/scrumretro/web/controller/ProjectController.java
+++ scrumretro-maven/src/main/java/com/scrumretro/web/controller/ProjectController.java
@@ -1,9 +1,12 @@
 package com.scrumretro.web.controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  *  REST or MVC Controller.
  *
  */
+@Controller
 public class ProjectController {
 
 }
