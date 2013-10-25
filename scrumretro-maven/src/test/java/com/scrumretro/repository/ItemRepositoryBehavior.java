79104115750f0d02dbb13674a171623696fbb21b
scrumretrospective <scrumretrospective@gmail.com>
2013-10-25 13:34:06 -0400

    to repo

--- scrumretro-maven/src/test/java/com/scrumretro/repository/ItemRepositoryBehavior.java
+++ scrumretro-maven/src/test/java/com/scrumretro/repository/ItemRepositoryBehavior.java
@@ -1,5 +1,60 @@
 package com.scrumretro.repository;
 
-public class ItemRepositoryBehavior {
+import static com.lordofthejars.nosqlunit.mongodb.MongoDbRule.MongoDbRuleBuilder.newMongoDbRule;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import com.foursquare.fongo.Fongo;
+import com.lordofthejars.nosqlunit.mongodb.MongoDbRule;
+import com.mongodb.Mongo;
+
+public class ItemRepositoryBehavior {
+	
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	@Rule
+	public MongoDbRule mongoDbRule = newMongoDbRule().defaultSpringMongoDb(
+			"scrumretro-test");
+
+	@Autowired
+	private ItemRepository itemRepository;
+
+	@Before
+	public void setUp(){
+		itemRepository.deleteAll();
+	}
+	
+	
+	
+
+	@Configuration
+	@EnableMongoRepositories
+	@ComponentScan(basePackageClasses = { ItemRepository.class })
+	@PropertySource("classpath:application.properties")
+	static class MongoConfiguration extends AbstractMongoConfiguration {
+
+		@Override
+		protected String getDatabaseName() {
+			return "scrumretro-test";
+		}
+
+		@Override
+		public Mongo mongo() {
+			return new Fongo("mongo-test").getMongo();
+		}
+
+		@Override
+		protected String getMappingBasePackage() {
+			return "com.scrumretro.repository.mongo";
+		}
+	}
 }
