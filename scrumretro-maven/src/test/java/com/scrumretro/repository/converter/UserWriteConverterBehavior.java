370a0fd935a9643735ffba81ac3c19b0bf291328
Sanju Thomas <sanju@sanju.org>
2013-10-26 11:34:01 -0400

    user converter

--- scrumretro-maven/src/test/java/com/scrumretro/repository/converter/UserWriteConverterBehavior.java
+++ scrumretro-maven/src/test/java/com/scrumretro/repository/converter/UserWriteConverterBehavior.java
@@ -1,6 +1,37 @@
 package com.scrumretro.repository.converter;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.mongodb.DBObject;
+import com.scrumretro.repository.model.User;
+
+/**
+ * 
+ * @author Sanju Thomas
+ *
+ */
 public class UserWriteConverterBehavior {
 
+	private UserWriteConverter userWriteConverter;
+	
+	@Before
+	public void setUp(){
+		userWriteConverter = new UserWriteConverter();
+	}
+	
+	@Test
+	public void shouldConvertUserCorrectly(){
+		final DBObject dbObject = userWriteConverter.convert(createUser());
+		assertEquals("info@scrumretro.com", dbObject.get("_id"));
+	}
+	
+	private User createUser(){
+		final User user = new User();
+		user.setEmailId("info@scrumretro.com");
+		return user;
+	}
 	
 }
