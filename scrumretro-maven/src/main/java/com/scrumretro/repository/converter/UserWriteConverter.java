81a4f62bb6eba7bc0de7339aabf0a895415855c8
scrumretrospective <scrumretrospective@gmail.com>
2013-10-26 11:34:01 -0400

    user converter

--- scrumretro-maven/src/main/java/com/scrumretro/repository/converter/UserWriteConverter.java
+++ scrumretro-maven/src/main/java/com/scrumretro/repository/converter/UserWriteConverter.java
@@ -2,14 +2,22 @@ package com.scrumretro.repository.converter;
 
 import org.springframework.core.convert.converter.Converter;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 import com.scrumretro.repository.model.User;
 
+/**
+ * 
+ * @author Sanju Thomas
+ *
+ */
 public class UserWriteConverter implements Converter<User, DBObject> {
 
 	public DBObject convert(final User user) {
-		// TODO Auto-generated method stub
-		return null;
+		DBObject dbo = new BasicDBObject();
+		dbo.put("_id", user.getEmailId());
+		dbo.put("userDetail", user.getUserDetail());
+		return dbo;
 	}
 
 }
