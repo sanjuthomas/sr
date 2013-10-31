235f384d947a853b7076e56ceee79b1876f222e3
Sanju Thomas <sanju@sanju.org>
2013-10-31 13:01:46 -0400

    adding more search metods

--- scrumretro-maven/src/main/java/com/scrumretro/repository/ProjectRepository.java
+++ scrumretro-maven/src/main/java/com/scrumretro/repository/ProjectRepository.java
@@ -1,8 +1,12 @@
 package com.scrumretro.repository;
 
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.scrumretro.repository.model.Project;
+import com.scrumretro.repository.model.User;
 
 /**
  * 
@@ -12,4 +16,9 @@ import com.scrumretro.repository.model.Project;
 public interface ProjectRepository extends PagingAndSortingRepository<Project, String> {
 
 	public Project findByName(final String name);
+	
+	public List<Project> findByUser(final User user);
+	
+	@Query("{ 'user._id' : ?0 }")
+	public List<Project> findByUserId(final String userId);
 }
--- scrumretro-maven/src/main/java/com/scrumretro/repository/RetrospectiveRepository.java
+++ scrumretro-maven/src/main/java/com/scrumretro/repository/RetrospectiveRepository.java
@@ -1,7 +1,11 @@
 package com.scrumretro.repository;
 
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import com.scrumretro.repository.model.Project;
 import com.scrumretro.repository.model.Retrospective;
 
 /**
@@ -9,5 +13,10 @@ import com.scrumretro.repository.model.Retrospective;
  * 
  */
 public interface RetrospectiveRepository extends PagingAndSortingRepository<Retrospective, String> {
+	
+	public List<Retrospective> findByProject(final Project project);
+	
+	@Query("{ 'project._id' : ?0 }")
+	public List<Retrospective> findByProjectId(final String projectId);
 	
 }
