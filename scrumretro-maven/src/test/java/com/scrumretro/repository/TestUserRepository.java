7d80e64d1084ff9ef03bcd0315526dcb69d2245d
Sanju Thomas <sanju@sanju.org>
2013-10-31 16:55:11 -0400

    rearanging testcases for maven

--- scrumretro-maven/src/test/java/com/scrumretro/repository/TestUserRepository.java
+++ scrumretro-maven/src/test/java/com/scrumretro/repository/TestUserRepository.java
@@ -1,13 +1,8 @@
 package com.scrumretro.repository;
 
 import static com.lordofthejars.nosqlunit.mongodb.MongoDbRule.MongoDbRuleBuilder.newMongoDbRule;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
-import java.util.List;
-
-import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,10 +18,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.foursquare.fongo.Fongo;
 import com.lordofthejars.nosqlunit.annotation.ShouldMatchDataSet;
-import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
 import com.lordofthejars.nosqlunit.mongodb.MongoDbRule;
 import com.mongodb.Mongo;
-import com.scrumretro.repository.model.Project;
 import com.scrumretro.repository.model.User;
 import com.scrumretro.repository.model.UserDetail;
 
@@ -35,10 +28,9 @@ import com.scrumretro.repository.model.UserDetail;
  * @author Sanju Thomas
  * 
  */
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
-public class TestProjectRepositoryBehavior {
+public class TestUserRepository {
 
 	@Autowired
 	private ApplicationContext applicationContext;
@@ -48,55 +40,23 @@ public class TestProjectRepositoryBehavior {
 			"scrumretro-test");
 
 	@Autowired
-	private ProjectRepository projectRepository;
+	private UserRepository userRepository;
 	
-
-//	@Test
-	@ShouldMatchDataSet(location = "/testData/project/project-p1.json")
-	public void shouldSaveProject() {
-		projectRepository.save(createProject());
+	@Before
+	public void setUp(){
+		this.userRepository.deleteAll();
 	}
-	
-	@Test
-	@UsingDataSet(locations = {"/testData/project/project-p1.json"})
-	public void shouldFindByName(){
-		final Project project = projectRepository.findByName("p1");
-		assertNotNull(project);
-		assertEquals("p1", project.getName());
-	}
-	
-	@Test
-	@UsingDataSet(locations = {"/testData/project/project-p1.json"})
-	public void shouldFindByUserId(){
-		final List<Project> projects = projectRepository.findByUserId("info@scrumretro.com");
-		assertNotNull(projects);
-		assertTrue(projects.size() > 0);
-		assertEquals("p1", projects.get(0).getName());
-	}
-	
 
 	@Test
-	@UsingDataSet(locations = {"/testData/project/project-p1.json"})
-	public void shouldFindByUser(){
-		final List<Project> projects = projectRepository.findByUser(createUser());
-		assertNotNull(projects);
-		assertTrue(projects.size() > 0);
-		assertEquals("p1", projects.get(0).getName());
-	}
-	
-	private Project createProject() {
-		final Project project = new Project();
-		project.setName("p1");
-		project.setDescription("This is a test project called p1");
-		project.setOrganization("o1");
-		final User user = createUser();
-		project.setUser(user);
-		return project;
+	@ShouldMatchDataSet(location = "/testData/user/user-u1.json")
+	public void shouldSaveUser() {
+		userRepository.save(createUser());
 	}
 	
 	private User createUser(){
 		final User user = new User();
 		user.setEmailId("info@scrumretro.com");
+		user.setPassword("password");
 		final UserDetail userDetail = new UserDetail();
 		userDetail.setFirstName("firstName");
 		userDetail.setLastName("lastName");
@@ -107,7 +67,7 @@ public class TestProjectRepositoryBehavior {
 
 	@Configuration
 	@EnableMongoRepositories
-	@ComponentScan(basePackageClasses = { ProjectRepository.class })
+	@ComponentScan(basePackageClasses = { UserRepository.class })
 	@PropertySource("classpath:application.properties")
 	static class MongoConfiguration extends AbstractMongoConfiguration {
 
@@ -125,5 +85,7 @@ public class TestProjectRepositoryBehavior {
 		protected String getMappingBasePackage() {
 			return "com.scrumretro.repository.mongo";
 		}
+
 	}
+
 }
