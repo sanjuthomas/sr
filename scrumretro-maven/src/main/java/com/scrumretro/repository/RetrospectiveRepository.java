4a7c4ab0bf517f7004ca864dbdeed3354d5ef594
scrumretrospective <scrumretrospective@gmail.com>
2013-10-31 19:42:19 -0400

    fixing testcases

--- scrumretro-maven/src/main/java/com/scrumretro/repository/RetrospectiveRepository.java
+++ scrumretro-maven/src/main/java/com/scrumretro/repository/RetrospectiveRepository.java
@@ -2,7 +2,6 @@ package com.scrumretro.repository;
 
 import java.util.List;
 
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.scrumretro.repository.model.Retrospective;
@@ -13,10 +12,8 @@ import com.scrumretro.repository.model.Retrospective;
  */
 public interface RetrospectiveRepository extends PagingAndSortingRepository<Retrospective, String> {
 	
-	@Query("{ 'project._id' : ?0 }")
 	public List<Retrospective> findByProjectId(final String projectId);
 	
-	@Query("{ 'user._id' : ?0 }")
 	public List<Retrospective> findByUserId(final String userId);
 	
 }
