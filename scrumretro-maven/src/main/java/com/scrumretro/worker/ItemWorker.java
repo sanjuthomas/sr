392dcc151f90918f917aaacc99d21befeb7b3ce2
Sanju Thomas <sanju@sanju.org>
2013-11-04 15:15:48 -0500

    item worker

--- scrumretro-maven/src/main/java/com/scrumretro/worker/ItemWorker.java
+++ scrumretro-maven/src/main/java/com/scrumretro/worker/ItemWorker.java
@@ -1,10 +1,53 @@
 package com.scrumretro.worker;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.scrumretro.repository.ItemRepository;
+import com.scrumretro.repository.RetrospectiveRepository;
+import com.scrumretro.repository.UserRepository;
+import com.scrumretro.repository.model.Item;
+
 /**
  * 
  * @author Sanju Thomas
  *
  */
 public class ItemWorker {
+	
+	@Autowired
+	private ItemRepository itemRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private RetrospectiveRepository retrospectiveRepository;
+	
+	
+	public void setRetrospectiveRepository(
+			final RetrospectiveRepository retrospectiveRepository) {
+		this.retrospectiveRepository = retrospectiveRepository;
+	}
 
+	public void setItemRepository(final ItemRepository itemRepository) {
+		this.itemRepository = itemRepository;
+	}
+
+	public void setUserRepository(final UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	public Item findById(final String id){
+		return null;
+	}
+	
+	public List<Item> findByRetrospectiveId(final String projectId){
+		return null;
+	}
+	
+	public List<Item> findByUserId(final String projectId){
+		return null;
+	}
 }
