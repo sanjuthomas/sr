55bbe4cc9955cd39e4c9630c132549d58ee31703
Sanju Thomas <sanju@sanju.org>
2013-11-04 15:15:48 -0500

    item worker

---
+++ scrumretro-maven/src/test/java/com/scrumretro/worker/TestItemWorker.java
@@ -0,0 +1,38 @@
+package com.scrumretro.worker;
+
+import org.junit.Before;
+import org.mockito.Mock;
+
+import com.scrumretro.repository.ItemRepository;
+import com.scrumretro.repository.RetrospectiveRepository;
+import com.scrumretro.repository.UserRepository;
+
+/**
+ * 
+ * @author Sanju Thomas
+ *
+ */
+public class TestItemWorker {
+	
+	private ItemWorker itemWorker;
+	
+	@Mock
+	private ItemRepository itemRepository;
+	
+	@Mock
+	private UserRepository userRepository;
+	
+	@Mock
+	private RetrospectiveRepository retrospectiveRepository;
+	
+	@Before
+	public void setUp(){
+		itemWorker = new ItemWorker();
+		itemWorker.setItemRepository(itemRepository);
+		itemWorker.setRetrospectiveRepository(retrospectiveRepository);
+		itemWorker.setUserRepository(userRepository);
+	}
+	
+	
+
+}
