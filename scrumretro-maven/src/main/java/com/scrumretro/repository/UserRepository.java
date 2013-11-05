14aeeb3754d7e2558c0d40432eba2b6f53059fd2
unknown <501737911@CORP-3F784Q1.nam.corp.ge.com>
2013-11-04 23:13:12 -0500

    User registration

--- scrumretro-maven/src/main/java/com/scrumretro/repository/UserRepository.java
+++ scrumretro-maven/src/main/java/com/scrumretro/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package com.scrumretro.repository;
 
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.scrumretro.repository.model.User;
@@ -11,4 +14,14 @@ import com.scrumretro.repository.model.User;
  */
 public interface UserRepository extends PagingAndSortingRepository<User, String> {
 
+	@Query("{ emailId: ?0, password : ?1 }")
+	public User findByUserIdAndPassword(final String emailId,final String password);
+	
+	public User findByEmailId(final String emailId);
+		
+	@Query("{ emailId : ?0 }")
+	public User findByUserId(final String emailId);
+	
+	@Query("{ active : true }")
+	public List<User> findActiveList();
 }
