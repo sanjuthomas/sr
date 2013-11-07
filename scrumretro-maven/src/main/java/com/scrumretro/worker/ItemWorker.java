1727d31b6d2fa813de08f447027c9805766a908e
Sanju Thomas <sanju@sanju.org>
2013-11-06 23:25:40 -0500

    item worker test

--- scrumretro-maven/src/main/java/com/scrumretro/worker/ItemWorker.java
+++ scrumretro-maven/src/main/java/com/scrumretro/worker/ItemWorker.java
@@ -6,9 +6,11 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.scrumretro.repository.ItemRepository;
+import com.scrumretro.repository.ProjectRepository;
 import com.scrumretro.repository.RetrospectiveRepository;
 import com.scrumretro.repository.UserRepository;
 import com.scrumretro.repository.model.Item;
+import com.scrumretro.repository.model.Project;
 import com.scrumretro.repository.model.Retrospective;
 import com.scrumretro.repository.model.User;
 import com.scrumretro.web.model.ItemResponse;
@@ -29,6 +31,9 @@ public class ItemWorker {
 	@Autowired
 	private RetrospectiveRepository retrospectiveRepository;
 	
+	@Autowired
+	private ProjectRepository projectRepository;
+	
 	
 	public void setRetrospectiveRepository(
 			final RetrospectiveRepository retrospectiveRepository) {
@@ -42,15 +47,20 @@ public class ItemWorker {
 	public void setUserRepository(final UserRepository userRepository) {
 		this.userRepository = userRepository;
 	}
+	
+	public void setProjectRepository(final ProjectRepository projectRepository) {
+		this.projectRepository = projectRepository;
+	}
 
+	/**
+	 * This method shall take an item id and construct ItemReponse object for UI.
+	 * 
+	 * @param id
+	 * @return
+	 */
 	public ItemResponse findById(final String id){
 		final Item item = itemRepository.findById(id);
-		final User user = userRepository.findByUserId(item.getUserId());
-		final Retrospective retrospective = retrospectiveRepository.findById(item.getRetrospectiveId());
-		final ItemResponse itemResponse = new ItemResponse();
-		BeanUtils.copyProperties(item, itemResponse);
-		
-		return null;
+		return createItemResponse(item);
 	}
 	
 	public List<Item> findByRetrospectiveId(final String projectId){
@@ -59,5 +69,25 @@ public class ItemWorker {
 	
 	public List<Item> findByUserId(final String projectId){
 		return null;
+	}
+	
+	/**
+	 * Load all the related documents and populate the UI model object.
+	 * 
+	 * @param item
+	 * @return
+	 */
+	private ItemResponse createItemResponse(final Item item){
+		
+		final User user = userRepository.findByUserId(item.getUserId());
+		final Retrospective retrospective = retrospectiveRepository.findById(item.getRetrospectiveId());
+		final Project project = projectRepository.findById(retrospective.getProjectId());
+		final ItemResponse itemResponse = new ItemResponse();
+		BeanUtils.copyProperties(item, itemResponse);
+		itemResponse.setOwnerDispalyName(user.getDisplayName());
+		itemResponse.setItemTypeDisplayString(item.getItemType().getDisplayString());
+		itemResponse.setProjectName(project.getName());
+		itemResponse.setRetrospectiveName(retrospective.getName());
+		return itemResponse;
 	}
 }
