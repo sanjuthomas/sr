0dadee8a28f77a839603083b53a68b4f378b84c4
scrumretrospective <scrumretrospective@gmail.com>
2013-11-11 18:03:47 -0500

    https://github.com/scrumretrospective/sr/issues/1

--- scrumretro-maven/src/main/java/com/scrumretro/web/service/ProjectService.java
+++ scrumretro-maven/src/main/java/com/scrumretro/web/service/ProjectService.java
@@ -1,17 +1,43 @@
 package com.scrumretro.web.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.scrumretro.web.model.ProjectResponse;
+import com.scrumretro.worker.ProjectWorker;
 
 /**
- *  Service class for Project object.
- *  
- *  @author Sanju Thomas
- *
+ * This class encapsulate all the service methods associate with Project
+ * document.
+ * 
+ * @author Sanju Thomas
+ * 
  */
 @Controller
 public class ProjectService {
-	
-	
+
+	@Autowired
+	private ProjectWorker projectWorker;
+
+	public void setProjectWorker(final ProjectWorker projectWorker) {
+		this.projectWorker = projectWorker;
+	}
+
+	/**
+	 * This method shall take project id request parameter and return the
+	 * ProjectResponse in json format.
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "/project/findById/{id}", method = RequestMethod.GET)
+	@ResponseBody
+	public ProjectResponse findById(@PathVariable("id") final String id) {
+		return projectWorker.findById(id);
+	}
 
 }
- 	
