051976130d729b93367daa8d41ce2b66eca3fe8a
Sanju Thomas <sanju@sanju.org>
2013-11-11 22:46:57 -0500

    https://github.com/scrumretrospective/sr/issues/1

--- scrumretro-maven/src/main/java/com/scrumretro/rest/Response.java
+++ scrumretro-maven/src/main/java/com/scrumretro/rest/Response.java
@@ -14,18 +14,9 @@ import com.scrumretro.enums.ResponseStatus;
 
 public class Response {
 
-	private ResponseStatus responseStatus = ResponseStatus.SUCCESS;
 
 	public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(
 			MediaType.APPLICATION_JSON.getType(),
 			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
-
-	public ResponseStatus getResponseStatus() {
-		return responseStatus;
-	}
-
-	public void setResponseStatus(final ResponseStatus responseStatus) {
-		this.responseStatus = responseStatus;
-	}
 
 }
