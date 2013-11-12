1f278dc4089b28c076ba97475d64a7c631536b14
Sanju Thomas <sanju@sanju.org>
2013-11-11 23:16:45 -0500

    https://github.com/scrumretrospective/sr/issues/1

--- scrumretro-maven/src/main/java/com/scrumretro/enums/ResponseStatus.java
+++ scrumretro-maven/src/main/java/com/scrumretro/enums/ResponseStatus.java
@@ -1,36 +1,33 @@
 package com.scrumretro.enums;
 
 /**
+ * In addition to org.springframework.http.HttpStatus we need few more custom
+ * application related status codes.
+ * 
  * 
  * @author Sanju Thomas
- *
+ * 
  */
 public enum ResponseStatus {
-	
-	DUPLICATE_VOTE("DUPLICATE_VOTE", 1901),
-	INACTIVE_ACCOUNT("INACTIVE_ACCOUNT", 1900);
-	
-	private String messageKey;
-	
-	private Integer errorCode;
-	
-	public String getMessageKey() {
-		return messageKey;
-	}
-	public void setMessageKey(final String messageKey) {
-		this.messageKey = messageKey;
-	}
-	public Integer getErrorCode() {
-		return errorCode;
-	}
-	public void setErrorCode(final Integer errorCode) {
-		this.errorCode = errorCode;
-	}
-	
 
-	private ResponseStatus(final String messageKey, final Integer errorCode){
-		this.messageKey = messageKey;
-		this.errorCode = errorCode;
-	}		
-	
+	DUPLICATE_VOTE(1901, "Duplicate Vote"), INACTIVE_ACCOUNT(1900,
+			"Inactive Account");
+
+	private final int value;
+
+	private final String reasonPhrase;
+
+	public int getValue() {
+		return value;
+	}
+
+	public String getReasonPhrase() {
+		return reasonPhrase;
+	}
+
+	private ResponseStatus(final int value, final String reasonPhrase) {
+		this.value = value;
+		this.reasonPhrase = reasonPhrase;
+	}
+
 }
