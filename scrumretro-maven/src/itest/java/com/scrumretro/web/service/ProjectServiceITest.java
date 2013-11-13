7966b0f658928b67cc369412eee3351344a77f7d
Sanju Thomas <sanju@sanju.org>
2013-11-13 18:17:40 -0500

    https://github.com/scrumretrospective/sr/issues/7

--- scrumretro-maven/src/itest/java/com/scrumretro/web/service/ProjectServiceITest.java
+++ scrumretro-maven/src/itest/java/com/scrumretro/web/service/ProjectServiceITest.java
@@ -1,10 +1,80 @@
 package com.scrumretro.web.service;
 
+import static com.lordofthejars.nosqlunit.mongodb.MongoDbRule.MongoDbRuleBuilder.newMongoDbRule;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.foursquare.fongo.Fongo;
+import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
+import com.lordofthejars.nosqlunit.mongodb.MongoDbRule;
+import com.mongodb.Mongo;
+import com.scrumretro.repository.ProjectRepository;
+import com.scrumretro.worker.ProjectWorker;
+
 /**
  * 
  * @author Sanju Thomas
  *
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
 public class ProjectServiceITest {
 
+
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	@Rule
+	public MongoDbRule mongoDbRule = newMongoDbRule().defaultSpringMongoDb("scrumretro-test");
+
+	@Autowired
+	private ProjectService projectService;
+	
+	@Before
+	public void setUp(){
+		
+	}
+	
+	@Test
+	@UsingDataSet(locations = {"/testData/item/item-i1.json"})
+	public void shouldFindProjectById(){
+		System.out.println(projectService);
+	}
+	
+	
+	
+	@Configuration
+	@EnableMongoRepositories
+	@ComponentScan(basePackageClasses = { ProjectRepository.class, ProjectService.class, ProjectWorker.class })
+	@PropertySource("classpath:application.properties")
+	static class MongoConfiguration extends AbstractMongoConfiguration {
+
+		@Override
+		protected String getDatabaseName() {
+			return "scrumretro-test";
+		}
+
+		@Override
+		public Mongo mongo() {
+			return new Fongo("mongo-test").getMongo();
+		}
+
+		@Override
+		protected String getMappingBasePackage() {
+			return "com.scrumretro.repository.mongo";
+		}
+		
+	}
 }
