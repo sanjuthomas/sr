a53464fb3bcf8f4abfd81866bccaf699e7296a3f
Sanju Thomas <sanju@sanju.org>
2013-11-14 17:07:40 -0500

    https://github.com/scrumretrospective/sr/issues/7

--- scrumretro-maven/src/test/java/com/scrumretro/repository/TestRetrospectiveRepository.java
+++ scrumretro-maven/src/test/java/com/scrumretro/repository/TestRetrospectiveRepository.java
@@ -1,13 +1,11 @@
 package com.scrumretro.repository;
 
-import static com.lordofthejars.nosqlunit.mongodb.MongoDbRule.MongoDbRuleBuilder.newMongoDbRule;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +21,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.foursquare.fongo.Fongo;
 import com.lordofthejars.nosqlunit.annotation.ShouldMatchDataSet;
 import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
-import com.lordofthejars.nosqlunit.mongodb.MongoDbRule;
 import com.mongodb.Mongo;
 import com.scrumretro.repository.model.Retrospective;
 
@@ -34,14 +31,10 @@ import com.scrumretro.repository.model.Retrospective;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
-public class TestRetrospectiveRepository {
+public class TestRetrospectiveRepository extends BaseUnitTest{
 	
 	@Autowired
 	private ApplicationContext applicationContext;
-
-	@Rule
-	public MongoDbRule mongoDbRule = newMongoDbRule().defaultSpringMongoDb(
-			"scrumretro-test");
 
 	@Autowired
 	private RetrospectiveRepository retrospectiveRepository;
