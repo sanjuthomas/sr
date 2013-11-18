96f78c931697d2220b295ff477b7b7658ca11e6b
Sanju Thomas <sanju@sanju.org>
2013-11-18 15:31:19 -0500

    more functionality to project

--- scrumretro-maven/src/main/java/com/scrumretro/repository/model/Project.java
+++ scrumretro-maven/src/main/java/com/scrumretro/repository/model/Project.java
@@ -9,7 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
  *
  */
 @Document(collection = "project")
-public class Project {
+public class Project{
 	
 	@Id
 	private String id;
--- scrumretro-maven/src/test/java/com/scrumretro/test/BaseUnitTest.java
+++ scrumretro-maven/src/test/java/com/scrumretro/test/BaseUnitTest.java
@@ -2,6 +2,10 @@ package com.scrumretro.test;
 
 import static com.lordofthejars.nosqlunit.mongodb.MongoDbRule.MongoDbRuleBuilder.newMongoDbRule;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Rule;
 
 import com.lordofthejars.nosqlunit.mongodb.MongoDbRule;
@@ -10,5 +14,36 @@ public class BaseUnitTest {
 	
 	@Rule
 	public MongoDbRule mongoDbRule = newMongoDbRule().defaultSpringMongoDb("scrumretro-test");
+	
+	/**
+	 * Convert the given json file to an object of given type.
+	 * 
+	 * @param jsonFile
+	 * @param clazz
+	 * @return
+	 */
+	protected Object toObject(final File jsonFile, final Class<?> clazz) {
+		final ObjectMapper mapper = new ObjectMapper();
+		try {
+			return mapper.readValue(jsonFile, clazz);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	/**
+	 * Convert the given object to json
+	 * 
+	 * @param object
+	 * @return
+	 */
+	protected String toJSON(final Object object){
+		final ObjectMapper mapper = new ObjectMapper();
+		try {
+			return mapper.writeValueAsString(object);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 }
