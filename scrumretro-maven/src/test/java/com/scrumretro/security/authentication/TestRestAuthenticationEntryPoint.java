4e498a57b7f1d5643920e6afdd6ba9722f36606c
unknown <501737911@CORP-3F784Q1.nam.corp.ge.com>
2013-11-18 21:34:06 -0500

    Added secutity authetication entry point impl

---
+++ scrumretro-maven/src/test/java/com/scrumretro/security/authentication/TestRestAuthenticationEntryPoint.java
@@ -0,0 +1,34 @@
+package com.scrumretro.security.authentication;
+
+import static junit.framework.Assert.assertEquals;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.AuthenticationException;
+
+public class TestRestAuthenticationEntryPoint {
+
+	private RestAuthenticationEntryPoint authenticationEntryPoint;
+
+    @Before
+    public void setUp() {
+        authenticationEntryPoint = new RestAuthenticationEntryPoint();
+    }
+
+    @Test
+    public void commence() throws IOException, ServletException {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        AuthenticationException ex = new AuthenticationCredentialsNotFoundException("");
+        authenticationEntryPoint.commence(request, response, ex);
+        assertEquals(HttpServletResponse.SC_UNAUTHORIZED, response.getStatus());
+    }
+}
