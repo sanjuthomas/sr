80d641c42531b041dad6ea36910a437cd4afee5b
unknown <501737911@CORP-3F784Q1.nam.corp.ge.com>
2013-11-19 22:15:50 -0500

    Authetication handler impl added

---
+++ scrumretro-maven/src/test/java/com/scrumretro/security/authentication/TestRestAuthenticationSuccessHandler.java
@@ -0,0 +1,38 @@
+package com.scrumretro.security.authentication;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * @author Ragil 
+ */
+public class TestRestAuthenticationSuccessHandler {
+
+    private RestAuthenticationSuccessHandler successHandler;
+
+    @Before
+    public void setUp() {
+        successHandler = new RestAuthenticationSuccessHandler();
+    }
+
+    @Test
+    public void onAuthenticationSuccess() throws ServletException, IOException {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        Authentication authentication = new UsernamePasswordAuthenticationToken(null, null);
+
+        successHandler.onAuthenticationSuccess(request, response, authentication);
+
+        assertEquals(MockHttpServletResponse.SC_OK, response.getStatus());
+    }
+}
+
