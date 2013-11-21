002ae799113749cab5693f0263b8ab79cec98a1d
sanjuthomas <sanju@sanju.org>
2013-11-21 00:13:26 -0500

    checking in test data generator

--- scrumretro-maven/src/test/java/com/scrumretro/web/model/TestUserRequest.java
+++ scrumretro-maven/src/test/java/com/scrumretro/web/model/TestUserRequest.java
@@ -9,30 +9,33 @@ import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 
+import org.databene.benerator.anno.Source;
+import org.databene.feed4junit.Feeder;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * 
  * @author Sanju Thomas
  * 
  */
+@RunWith(Feeder.class)
 public class TestUserRequest {
 
 	private Validator validator;
-	
-	private UserRequest userRequest;
 
 	@Before
-	public void setUp() {
+	public void setUp(){
 		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
 		validator = factory.getValidator();
-		userRequest = new UserRequest();
 	}
-	
+
 	@Test
-	public void shouldValidateAllNotEmpty(){
-		final Set<ConstraintViolation<UserRequest>> validtionResults = validator.validate(userRequest);
+	public void shouldValidateInvalidEntries(@Source("src/test/resources/testData/userRequest/invalid.csv") UserRequest userRequest) {
+		final Set<ConstraintViolation<UserRequest>> validtionResults = validator
+				.validate(userRequest);
 		assertEquals(5, validtionResults.size());
 	}
+
 }
