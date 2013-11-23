55b8d5e67ddca2979cc64e0f9a7b3753b693b427
unknown <501737911@CORP-3F784Q1.nam.corp.ge.com>
2013-11-23 14:09:42 -0500

    Added Test case for authentication

---
+++ scrumretro-maven/src/main/java/com/scrumretro/web/service/UserService.java
@@ -0,0 +1,63 @@
+package com.scrumretro.web.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.scrumretro.security.authentication.ScrumretroUser;
+import com.scrumretro.web.model.UserResponse;
+import com.scrumretro.worker.UserWorker;
+
+/**
+ * This class encapsulate all the service methods associate with User
+ * document.
+ * 
+ * @author Ragil
+ * 
+ */
+@Controller
+public class UserService {
+
+	@Autowired
+	private UserWorker userWorker;
+
+	public UserWorker getUserWorker() {
+		return userWorker;
+	}
+
+	public void setUserWorker(UserWorker userWorker) {
+		this.userWorker = userWorker;
+	}
+	
+	/**
+	 * This service shall return the current user details
+	 * in json format.
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "/user/currentUser", method = RequestMethod.GET)
+	@ResponseBody
+	public UserResponse currentUser(final Authentication authentication) {
+		 final ScrumretroUser currentUser =  (ScrumretroUser)authentication.getPrincipal();
+		 final UserResponse userResponse = new UserResponse();
+		 BeanUtils.copyProperties(currentUser, userResponse);
+		 return userResponse;
+	}
+	/**
+	 * This service shall take user id request parameter and return the
+	 * UserResponse in json format.
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "/user/findById/{id}", method = RequestMethod.GET)
+	@ResponseBody
+	public UserResponse findByUserId(@PathVariable("id") final String id) {
+		return userWorker.findByUserId(id);
+	}
+}
---
+++ scrumretro-maven/src/test/java/com/scrumretro/security/authentication/TestScrumretroUserService.java
@@ -0,0 +1,79 @@
+package com.scrumretro.security.authentication;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.springframework.security.core.GrantedAuthority;
+
+import com.scrumretro.repository.UserRepository;
+import com.scrumretro.repository.model.User;
+import com.scrumretro.repository.model.UserDetail;
+
+public class TestScrumretroUserService {
+
+	ScrumretroUserService scrumretroUserService;
+	
+	@Mock
+	private UserRepository mockUserRepository;
+	
+	
+	@Before
+	public void setUp(){
+		initMocks(this);
+		final User user = createUser();
+		when(mockUserRepository.findByUserId(any(String.class))).thenReturn(user);
+		scrumretroUserService = new ScrumretroUserService();
+		scrumretroUserService.setUserRepository(mockUserRepository);
+	}
+	
+	@Test
+	public void shouldLoadUserByUsername(){
+		final ScrumretroUser scrumretroUser = (ScrumretroUser)scrumretroUserService.loadUserByUsername("info@scrumretro.com");
+		assertNotNull(scrumretroUser);
+		assertEquals("info@scrumretro.com", scrumretroUser.getUsername());
+		assertEquals("password", scrumretroUser.getPassword());
+		final Collection<GrantedAuthority> gaCol = scrumretroUser.getAuthorities();
+		assertNotNull(gaCol);
+		assertTrue(gaCol.size() > 0);
+		//final GrantedAuthority ga = ((List<GrantedAuthority>)gaCol).get(0);
+		final Iterator<GrantedAuthority> it = gaCol.iterator();
+		final String roles [] = {"ROLE_USER"};
+		while(it.hasNext()){
+			assertTrue(contains(roles, it.next().getAuthority()));
+		}
+		
+	}
+	
+	private Boolean contains(final String strArray [],final String str){
+		for(int i=0;i<strArray.length;i++){
+			
+			if(str.equals(strArray[i])){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private User createUser(){
+		final User user = new User();
+		user.setUserId("info@scrumretro.com");
+		user.setPassword("password");
+		user.setActive(true);
+		final UserDetail userDetail = new UserDetail();
+		userDetail.setFirstName("firstName");
+		userDetail.setLastName("lastName");
+		userDetail.setOrganization("organization");
+		user.setUserDetail(userDetail);
+		return user;
+	}
+}
