e98826daaceac16f17014018c7e9424c430e0f9e
sanjuthomas <sanju@sanju.org>
2013-11-26 14:02:25 -0500

    fixing testcase

--- scrumretro-maven/src/test/java/com/scrumretro/security/authentication/TestAuthentication.java
+++ scrumretro-maven/src/test/java/com/scrumretro/security/authentication/TestAuthentication.java
@@ -6,36 +6,38 @@ import static org.mockito.MockitoAnnotations.initMocks;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import javax.servlet.Filter;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import com.scrumretro.repository.UserRepository;
-import com.scrumretro.repository.model.User;
-import com.scrumretro.repository.model.UserDetail;
 import com.scrumretro.web.service.UserService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:test-applicationContext.xml","classpath:test-applicationSecurity.xml" })
 public class TestAuthentication {
 
-	@Autowired
-	private ApplicationContext applicationContext;
-	
-	@Autowired
-	private ScrumRetroUserDetailsAuthenticationProvider scrumRetroUserDetailsAuthenticationProvider;
+	@Mock
+	private AuthenticationManager mockAuthenticationManager;
 	
 	@Mock
-	private UserRepository mockUserRepository;
+	private Authentication mockAuthentication;
 	
 	private MockMvc mockMvc;
 
@@ -45,43 +47,41 @@ public class TestAuthentication {
 	@Autowired
     private FilterChainProxy springSecurityFilterChain;
 	
-	@Autowired
-    private UsernamePasswordAuthenticationFilter filter;
-	
 	@Before
 	public void setUp() {
 		initMocks(this);
-		when(mockUserRepository.findByUserId(any(String.class))).thenReturn(createUser());
-		scrumRetroUserDetailsAuthenticationProvider.setUserRepository(mockUserRepository);
+		when(mockAuthenticationManager.authenticate(any(Authentication.class))).thenAnswer(new Answer<Authentication>() {
+			 public Authentication answer(InvocationOnMock invocation) {
+			     final Object[] args = invocation.getArguments();
+			     final UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = 
+			    		 (UsernamePasswordAuthenticationToken)args[0];
+			     if(!"password".equals(usernamePasswordAuthenticationToken.getCredentials().toString())){
+			    	  throw new BadCredentialsException("incorrect password");
+			     }
+			     return mockAuthentication;
+			 }});
+		
+		for(final SecurityFilterChain securityFilterChain : springSecurityFilterChain.getFilterChains()){
+			for(final Filter filter : securityFilterChain.getFilters()){
+				if(filter instanceof UsernamePasswordAuthenticationFilter){
+					((UsernamePasswordAuthenticationFilter)filter).setAuthenticationManager(mockAuthenticationManager);
+				}
+			}
+		}
 		this.mockMvc = MockMvcBuilders.standaloneSetup(userService).addFilter(this.springSecurityFilterChain).build();
 	}
+	
 	@Test
-    public void successLogin() throws Exception {
+    public void shouldAuthenticate() throws Exception {
 		mockMvc.perform(post("/login").param("username", "info@scrumretro.com")
 			                    .param("password", "password")).andExpect(status().isOk());
 	}
 	
 	@Test
-    public void failLogin() throws Exception {
+    public void shouldNotAuthenticate() throws Exception {
 		mockMvc.perform(post("/login").param("username", "info@scrumretro.com")
 			                    .param("password", "passwordwrong")).andExpect(status().isUnauthorized());
 	}
-	
-
-	
-	private User createUser(){
-		final User user = new User();
-		user.setUserId("info@scrumretro.com");
-		user.setPassword("$2a$10$EzO6FnN.E1hFt18nI6BjfuRsQvavkeOWmC.PeUnaB54NpZDYXoGXC");
-		user.setActive(true);
-		final UserDetail userDetail = new UserDetail();
-		userDetail.setFirstName("firstName");
-		userDetail.setLastName("lastName");
-		userDetail.setOrganization("organization");
-		user.setUserDetail(userDetail);
-		return user;
-	}
-	
 	
 }
 
