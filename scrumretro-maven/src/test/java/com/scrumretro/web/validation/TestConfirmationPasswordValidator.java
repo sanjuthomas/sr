c8342bb1120a920dab7ac80f7fae3eb58aa1d4a4
sanjuthomas <sanju@sanju.org>
2013-12-09 19:19:23 -0500

    confirmation password validator

---
+++ scrumretro-maven/src/test/java/com/scrumretro/web/validation/TestConfirmationPasswordValidator.java
@@ -0,0 +1,17 @@
+package com.scrumretro.web.validation;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author Sanju Thomas
+ *
+ */
+public class TestConfirmationPasswordValidator {
+	
+	@Test
+	public void shouldValidateConfirmationPassword(){
+		
+	}
+
+}
