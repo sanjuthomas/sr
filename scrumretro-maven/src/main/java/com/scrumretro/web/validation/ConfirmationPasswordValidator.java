68599a3a4fff3e04584902b2257b97650d0de2b2
sanjuthomas <sanju@sanju.org>
2013-12-10 18:42:41 -0500

    reset password

--- scrumretro-maven/src/main/java/com/scrumretro/web/validation/ConfirmationPasswordValidator.java
+++ scrumretro-maven/src/main/java/com/scrumretro/web/validation/ConfirmationPasswordValidator.java
@@ -5,6 +5,7 @@ import javax.validation.ConstraintValidatorContext;
 
 import org.apache.commons.lang.StringUtils;
 
+import com.scrumretro.web.model.UserPasswordResetRequest;
 import com.scrumretro.web.model.UserRegistrationRequest;
 
 /**
@@ -12,14 +13,24 @@ import com.scrumretro.web.model.UserRegistrationRequest;
  * @author Sanju Thomas
  *
  */
-public class ConfirmationPasswordValidator implements ConstraintValidator<ValidateConfirmationPassword, UserRegistrationRequest> {
+public class ConfirmationPasswordValidator implements ConstraintValidator<ValidateConfirmationPassword, Object> {
 
 	@Override
 	public void initialize(ValidateConfirmationPassword constraintAnnotation) {
 	}
 
 	@Override
-	public boolean isValid(UserRegistrationRequest userRequest, ConstraintValidatorContext context) {
-		return StringUtils.equals(userRequest.getConfirmPassword(), userRequest.getPassword());
+	public boolean isValid(Object request, ConstraintValidatorContext context) {
+		Boolean isValid = false;
+		if(request instanceof UserRegistrationRequest){
+			final UserRegistrationRequest userRegistrationRequest = (UserRegistrationRequest)request;
+			isValid = StringUtils.equals(userRegistrationRequest.getConfirmPassword(),
+					userRegistrationRequest.getPassword());
+		}else if(request instanceof UserPasswordResetRequest){
+			final UserPasswordResetRequest userPasswordResetRequest = (UserPasswordResetRequest)request;
+			isValid = StringUtils.equals(userPasswordResetRequest.getNewconfirmPassword(),
+					userPasswordResetRequest.getNewPassword());
+		}
+		return isValid;
 	}
 }
