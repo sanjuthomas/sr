8ba9fd86a69b8c77d519ab9d5b833245a39b5cb3
sanjuthomas <sanju@sanju.org>
2013-12-12 19:14:57 -0500

    adding testcase for userservice

--- scrumretro-maven/src/test/java/com/scrumretro/web/service/TestUserService.java
+++ scrumretro-maven/src/test/java/com/scrumretro/web/service/TestUserService.java
@@ -1,7 +1,30 @@
 package com.scrumretro.web.service;
 
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.scrumretro.worker.UserWorker;
+
+/**
+ * 
+ * @author Sanju Thomas
+ *
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:test-applicationContext.xml" })
 public class TestUserService {
+	
+	@Mock
+	private UserWorker userWorker;
 
+	@Autowired
+	private UserService userService;
+	
+	
+	
 		
 }
