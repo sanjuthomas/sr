031610098fc9998fd491b02e275e911b6765cdf6
sanjuthomas <sanju@sanju.org>
2013-12-12 20:17:08 -0500

    adding testcase for userservice

--- scrumretro-maven/src/test/java/com/scrumretro/web/service/TestUserService.java
+++ scrumretro-maven/src/test/java/com/scrumretro/web/service/TestUserService.java
@@ -1,8 +1,13 @@
 package com.scrumretro.web.service;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.MockitoAnnotations.initMocks;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,6 +19,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.scrumretro.rest.Response;
 import com.scrumretro.web.model.UserResponse;
 import com.scrumretro.worker.UserWorker;
 
@@ -40,11 +46,19 @@ public class TestUserService {
 		initMocks(this);
 		this.mockMvc = MockMvcBuilders.standaloneSetup(userService).build();
 		when(userWorker.findByUserId(any(String.class))).thenReturn(createUserResponse());
+		userService.setUserWorker(userWorker);
 	}
 	
 	@Test
-	public void shouldFindByUserId(){
-		
+	public void shouldFindByUserId() throws Exception{
+		mockMvc.perform(get("/user/findById/{id}", "userId"))
+		.andExpect(status().isOk())
+		.andExpect(
+				content().contentType(Response.APPLICATION_JSON_UTF8))
+		.andExpect(jsonPath("$firstName", is("firstName")))
+		.andExpect(jsonPath("$lastName", is("lastName")))
+		.andExpect(jsonPath("$organization", is("organization")))
+		.andExpect(jsonPath("$userId", is("userId")));
 	}
 	
 	
