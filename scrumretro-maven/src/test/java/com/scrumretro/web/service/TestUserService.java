0419485b43464dbed48e85807cf9fcf6c739912b
sanjuthomas <sanju@sanju.org>
2013-12-13 00:29:11 -0500

    fixing testcases

--- scrumretro-maven/src/test/java/com/scrumretro/web/service/TestUserService.java
+++ scrumretro-maven/src/test/java/com/scrumretro/web/service/TestUserService.java
@@ -1,10 +1,11 @@
 package com.scrumretro.web.service;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.mockito.MockitoAnnotations.initMocks;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -20,6 +21,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.scrumretro.rest.Response;
+import com.scrumretro.web.model.UserRegistrationRequest;
 import com.scrumretro.web.model.UserResponse;
 import com.scrumretro.worker.UserWorker;
 
@@ -46,6 +48,7 @@ public class TestUserService {
 		initMocks(this);
 		this.mockMvc = MockMvcBuilders.standaloneSetup(userService).build();
 		when(userWorker.findByUserId(any(String.class))).thenReturn(createUserResponse());
+		when(userWorker.save(any(UserRegistrationRequest.class))).thenReturn(createUserResponse());
 		userService.setUserWorker(userWorker);
 	}
 	
@@ -58,7 +61,19 @@ public class TestUserService {
 		.andExpect(jsonPath("$firstName", is("firstName")))
 		.andExpect(jsonPath("$lastName", is("lastName")))
 		.andExpect(jsonPath("$organization", is("organization")))
-		.andExpect(jsonPath("$userId", is("userId")));
+		.andExpect(jsonPath("$userId", is("info@scumretro.com")));
+	}
+	
+	@Test
+	public void shouldRegisterUser() throws Exception{
+		this.mockMvc.perform(
+				post("/user/register/").content(createUserRegistrationRequest().toString())
+						.contentType(Response.APPLICATION_JSON_UTF8))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$firstName", is("firstName")))
+				.andExpect(jsonPath("$lastName", is("lastName")))
+				.andExpect(jsonPath("$organization", is("organization")))
+				.andExpect(jsonPath("$userId", is("info@scumretro.com")));
 	}
 	
 	
@@ -67,9 +82,18 @@ public class TestUserService {
 		userResponse.setFirstName("firstName");
 		userResponse.setLastName("lastName");
 		userResponse.setOrganization("organization");
-		userResponse.setUserId("userId");
+		userResponse.setUserId("info@scumretro.com");
 		return userResponse;
 	}
 	
-	
+	private UserRegistrationRequest createUserRegistrationRequest(){
+		final UserRegistrationRequest userRegistrationRequest = new UserRegistrationRequest();
+		userRegistrationRequest.setFirstName("firstName");
+		userRegistrationRequest.setLastName("lastName");
+		userRegistrationRequest.setOrganization("organization");
+		userRegistrationRequest.setPassword("password");
+		userRegistrationRequest.setConfirmPassword("password");
+		userRegistrationRequest.setUserId("info@scumretro.com");
+		return userRegistrationRequest;
+	}
 }
