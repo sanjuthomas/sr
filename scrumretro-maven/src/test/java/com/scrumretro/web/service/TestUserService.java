35f56fccb59795f84221828625ab4f831d458b67
sanjuthomas <sanju@sanju.org>
2013-12-12 19:53:24 -0500

    adding testcase for userservice

--- scrumretro-maven/src/test/java/com/scrumretro/web/service/TestUserService.java
+++ scrumretro-maven/src/test/java/com/scrumretro/web/service/TestUserService.java
@@ -1,12 +1,20 @@
 package com.scrumretro.web.service;
 
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.scrumretro.web.model.UserResponse;
 import com.scrumretro.worker.UserWorker;
 
 /**
@@ -25,11 +33,29 @@ public class TestUserService {
 	@Autowired
 	private UserService userService;
 	
+	private MockMvc mockMvc;
+	
+	@Before
+	public void setUp(){
+		initMocks(this);
+		this.mockMvc = MockMvcBuilders.standaloneSetup(userService).build();
+		when(userWorker.findByUserId(any(String.class))).thenReturn(createUserResponse());
+	}
 	
 	@Test
 	public void shouldFindByUserId(){
 		
 	}
 	
-		
+	
+	private UserResponse createUserResponse(){
+		final UserResponse userResponse = new UserResponse();
+		userResponse.setFirstName("firstName");
+		userResponse.setLastName("lastName");
+		userResponse.setOrganization("organization");
+		userResponse.setUserId("userId");
+		return userResponse;
+	}
+	
+	
 }
