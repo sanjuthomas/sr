67c4bbda78a287ebce346696df5e368b829bce75
sanjuthomas <sanju@sanju.org>
2014-01-03 18:17:44 -0500

    retro worker

---
+++ scrumretro-maven/src/main/java/com/scrumretro/web/model/RetrospectiveRequest.java
@@ -0,0 +1,63 @@
+package com.scrumretro.web.model;
+
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.joda.time.DateTime;
+
+import com.scrumretro.rest.Request;
+
+/**
+ * 
+ * @author Sanju Thomas
+ *
+ */
+public class RetrospectiveRequest extends Request {
+	
+	@Size(max=256)
+	private String id;
+	
+	@NotEmpty 
+	@Size(max=256)
+	private String projectId;
+	
+	@NotEmpty 
+	@Size(max=512)
+	private String name;
+	
+	@NotEmpty
+	private DateTime time;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(final String id) {
+		this.id = id;
+	}
+
+	public String getProjectId() {
+		return projectId;
+	}
+
+	public void setProjectId(final String projectId) {
+		this.projectId = projectId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	public DateTime getTime() {
+		return time;
+	}
+
+	public void setTime(final DateTime time) {
+		this.time = time;
+	}
+
+}
