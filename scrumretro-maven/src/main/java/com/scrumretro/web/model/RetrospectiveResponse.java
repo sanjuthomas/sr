c322e0b1e0786b11f8a81113d9d99c4814100ab3
sanjuthomas <sanju@sanju.org>
2014-01-03 22:58:42 -0500

    retro response

--- scrumretro-maven/src/main/java/com/scrumretro/web/model/RetrospectiveResponse.java
+++ scrumretro-maven/src/main/java/com/scrumretro/web/model/RetrospectiveResponse.java
@@ -7,4 +7,36 @@ import com.scrumretro.rest.Response;
  * @author Sanju Thomas
  *
  */
-public class RetrospectiveResponse extends Response{}
+public class RetrospectiveResponse extends Response{
+	
+	private String id;
+	
+	private String name;
+	
+	private String projectId;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(final String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	public String getProjectId() {
+		return projectId;
+	}
+
+	public void setProjectId(final String projectId) {
+		this.projectId = projectId;
+	}
+	
+}
