c0631c6feb2de5a1bf95816edc446daf30350d2a
sanjuthomas <sanju@sanju.org>
2014-01-08 22:27:23 -0500

    adding status to response

--- scrumretro-maven/src/main/java/com/scrumretro/rest/Response.java
+++ scrumretro-maven/src/main/java/com/scrumretro/rest/Response.java
@@ -16,4 +16,34 @@ public class Response {
 			MediaType.APPLICATION_JSON.getType(),
 			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
 	
+	private String status = "success";
+	
+	private String message;
+	
+	private Integer errorCode;
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(final String status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(final String message) {
+		this.message = message;
+	}
+
+	public Integer getErrorCode() {
+		return errorCode;
+	}
+
+	public void setErrorCode(final Integer errorCode) {
+		this.errorCode = errorCode;
+	}
+	
 }
