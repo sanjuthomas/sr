bd6a7998520930062de402ec5d4a4b7cc314d9fe
sanjuthomas <sanju@sanju.org>
2014-01-10 18:58:47 -0500

    adding item request

---
+++ scrumretro-maven/src/main/java/com/scrumretro/web/model/ItemRequest.java
@@ -0,0 +1,43 @@
+package com.scrumretro.web.model;
+
+import com.scrumretro.enums.ItemType;
+import com.scrumretro.rest.Request;
+
+/**
+ * 
+ * @author Sanju Thomas
+ *
+ */
+public class ItemRequest extends Request{
+	
+	private String retrospectiveId;
+	
+	private String description;
+	
+	private ItemType itemType;
+
+	public String getRetrospectiveId() {
+		return retrospectiveId;
+	}
+
+	public void setRetrospectiveId(final String retrospectiveId) {
+		this.retrospectiveId = retrospectiveId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(final String description) {
+		this.description = description;
+	}
+
+	public ItemType getItemType() {
+		return itemType;
+	}
+
+	public void setItemType(final ItemType itemType) {
+		this.itemType = itemType;
+	}
+
+}
