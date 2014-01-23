9b463eda3c21bc5c0410a3de030e6cef23bf6561
sanjuthomas <sanju@sanju.org>
2014-01-23 00:42:02 -0500

    adding retro

--- scrumretro-maven/src/test/java/com/scrumretro/worker/TestRetrospectiveWorker.java
+++ scrumretro-maven/src/test/java/com/scrumretro/worker/TestRetrospectiveWorker.java
@@ -1,16 +1,17 @@
 package com.scrumretro.worker;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mock;
-
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
 import com.scrumretro.repository.RetrospectiveRepository;
 import com.scrumretro.repository.model.Retrospective;
+import com.scrumretro.web.model.RetrospectiveRequest;
 import com.scrumretro.web.model.RetrospectiveResponse;
 
 /**
@@ -30,6 +31,7 @@ public class TestRetrospectiveWorker {
 		initMocks(this);
 		retrospectiveWorker = new RetrospectiveWorker();
 		when(mockRetrospectiveRepository.findById(any(String.class))).thenReturn(createRetrospective());
+		when(mockRetrospectiveRepository.save(any(Retrospective.class))).thenReturn(createRetrospective());
 		retrospectiveWorker.setRetrospectiveRepository(mockRetrospectiveRepository);
 		
 	}
@@ -42,12 +44,28 @@ public class TestRetrospectiveWorker {
 		assertEquals("p1", retrospectiveResponse.getProjectId());
 	}
 	
+	@Test
+	public void shouldSave(){
+		RetrospectiveResponse retrospectiveResponse = retrospectiveWorker.save(createRetrospectiveRequest());
+		assertEquals("r1", retrospectiveResponse.getId());
+		assertEquals("rname", retrospectiveResponse.getName());
+		assertEquals("p1", retrospectiveResponse.getProjectId());
+	}
+	
 	private Retrospective createRetrospective(){
 		final Retrospective retrospective = new Retrospective();
 		retrospective.setId("r1");
 		retrospective.setName("rname");
 		retrospective.setProjectId("p1");
 		return retrospective;
+	}
+	
+	private RetrospectiveRequest createRetrospectiveRequest(){
+		final RetrospectiveRequest retrospectiveRequest = new RetrospectiveRequest();
+		retrospectiveRequest.setId("r1");
+		retrospectiveRequest.setName("rname");
+		retrospectiveRequest.setProjectId("p1");
+		return retrospectiveRequest;
 	}
 
 }
