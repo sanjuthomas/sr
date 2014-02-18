d7e1ab45511ef32a83b9cf80b27c5f5d1e4bb516
sanjuthomas <sanju@sanju.org>
2014-02-18 14:37:32 -0500

    clean up web socket

--- scrumretro-maven/src/main/java/com/scrumretro/web/servlet/WebSocket.java
+++ scrumretro-maven/src/main/java/com/scrumretro/web/servlet/WebSocket.java
@@ -12,60 +12,57 @@ import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
+import com.scrumretro.exception.ScrumRetroRuntimeException;
 
+/**
+ * 
+ * @author Ragil
+ *
+ */
 @ServerEndpoint(value = "/connecttoserver/{username}")
 public class WebSocket {
 
-    
-    public static final Map<String,WebSocket> connections =
-            new HashMap<String,WebSocket>();
+	private static final Map<String, WebSocket> connections = new HashMap<String, WebSocket>();
 
-    
-    private Session session;
+	private Session session;
 
-    public WebSocket() {
-       
-    }
+	public WebSocket() {
 
+	}
 
-    public Session getSession() {
+	public Session getSession() {
 		return session;
 	}
 
-
-	public void setSession(Session session) {
+	public void setSession(final Session session) {
 		this.session = session;
 	}
 
-
 	@OnOpen
-    public void start(Session session,@PathParam("username") String userName) {
-    	this.session = session;
-    	connections.put(userName,this);
-    }
+	public void start(final Session session,
+			@PathParam("username") final String userName) {
+		this.session = session;
+		connections.put(userName, this);
+	}
 
+	@OnClose
+	public void end() {
+		connections.remove(this);
 
-    @OnClose
-    public void end() {
-        connections.remove(this);
-       
-    }
+	}
 
+	@OnMessage
+	public void incoming(final String message) {
 
-    @OnMessage
-    public void incoming(String message) {
-       
-    }
+	}
 
+	@OnError
+	public void onError(final Throwable t) throws Throwable {
+		throw new ScrumRetroRuntimeException("Error occurred in WebSocket", t);
+	}
 
-    @OnError
-    public void onError(Throwable t) throws Throwable {
-    	System.out.println("Chat Error: " + t.toString());
-    }
-
-    public void broadcast(String message) throws IOException{
-    	this.getSession().getBasicRemote().sendText(message);
-    }
-   
+	public void broadcast(final String message) throws IOException {
+		this.getSession().getBasicRemote().sendText(message);
+	}
 
 }
